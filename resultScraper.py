ea389fa6b3cec236ea812decb93f0f0666edf601
Abhijith <abhijith4m0505@gmail.com>
2016-08-14 14:02:02 +0530

    Updated code for better understanding

--- resultScraper.py
+++ resultScraper.py
@@ -1,4 +1,5 @@
 import urllib2
+import os.path
 from bs4 import BeautifulSoup
 from twilio.rest import TwilioRestClient
 
@@ -7,20 +8,25 @@ auth_token  = " "  # Your Auth Token from www.twilio.com/console
 
 vtu = "http://results.vtu.ac.in/"
 page = urllib2.urlopen(vtu)
-soup = BeautifulSoup(page)
+soup = BeautifulSoup(page,"html.parser")
 
 complete_text = soup.getText()
 
-file = open('rctmp','r+')
-
-flag = file.read(1)
+if os.path.isfile('rctmp') == True:
+	file = open('rctmp','r+')
+	flag = file.read(1)
+else:
+        file = open('rctmp','w')
+        file.write('0')
+        file = open('rctmp','r+')
+        flag = file.read(1)
 
 if flag == "0":
-    if complete_text.find(" ")!=-1: #The text you want to find, "B.E/B.Tech IV Semester" - For 4th sem results
-            client = TwilioRestClient(account_sid, auth_token)
-            message = client.messages.create(body="Hey! Your Results are out!",
-                            to=" ",   #Replace with your number to which you want to send the message (with Country Code)
-                            from_=" ") # Replace with your Twilio number
+	if complete_text.find(" ")!=-1:  #The text you want to search for | "B.E/B.Tech IV Semester" for 4th sem results
+		client = TwilioRestClient(account_sid, auth_token)
+         	message = client.messages.create(body=" ",		#Messgage body you want to receive as SMS
+				to=" ",   #Replace with your number to which you want to send the message
+				from_=" ") # Replace with your Twilio number
 
-            file = open('rctmp','w')
-            file.write('1')
+        file = open('rctmp','w')
+        file.write('1')
