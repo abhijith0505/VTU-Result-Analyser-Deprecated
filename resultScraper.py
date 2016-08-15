e729de0984840acbad9ecc4e1a165f42e61780d3
Abhijith <abhijith4m0505@gmail.com>
2016-08-15 14:02:55 +0530

    Build test | changes to .py

--- resultScraper.py
+++ resultScraper.py
@@ -5,7 +5,7 @@ from twilio.rest import TwilioRestClient
 
 account_sid = " " # Your Account SID from www.twilio.com/console
 auth_token  = " "  # Your Auth Token from www.twilio.com/console
-usn = " 1cd06cs123 " # Your USN 
+usn = "  " # Your USN 
 vtu = "http://results.vtu.ac.in/?submit=1&rid={}".format(usn)
 page = urllib2.urlopen(vtu)
 soup = BeautifulSoup(page,"html.parser")
