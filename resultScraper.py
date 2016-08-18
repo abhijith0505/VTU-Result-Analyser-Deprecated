75f8588b8f6adcb17a5f742aa58e8cb3edca6651
Sharath Huddar <sharath.huddar@gmail.com>
2016-08-18 21:50:34 +0530

    Add a class called student which gets all data from results page upon initialisation.

--- resultScraper.py
+++ resultScraper.py
@@ -2,35 +2,17 @@ import urllib2
 import os.path
 from lxml import etree, html
 import requests
-
+from student import Student
 
 #from twilio.rest import TwilioRestClient
 
 #account_sid = " " # Your Account SID from www.twilio.com/console
 #auth_token  = " "  # Your Auth Token from www.twilio.com/console
 usn = "1MV14IS045" # Your USN
-BASE_URL = 'http://results.vtu.ac.in'
 #using lxml to access data through xpath selectors
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Host': 'results.vtu.ac.in',
-    'Referer': 'http://results.vtu.ac.in/'
-}
-
-payload = {
-    'rid': usn,
-    'submit': 'SUBMIT'
-}
-
-response = requests.post(BASE_URL + '/vitavi.php', payload, headers=headers)
-
-tree = html.fromstring(response.content)
-
-student_name_usn = tree.xpath('/html/body/table/tbody/tr[3]/td[2]/table/tbody/tr[3]/td/table/tbody/tr[2]/td[1]/table[2]/tbody/tr/td/table/tbody/tr[2]/td/b/text()')
-
-
+Sharath = Student()
+print Sharath.get_name()
 '''
 if os.path.isfile('rctmp') == True:
 	file = open('rctmp','r+')
